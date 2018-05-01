3eed41f7d27a30b986fe5a24a896c5f604c83c82
ZatsepinMikhail <salamence1996@gmail.com>
2018-05-01 18:44:29 +0300

    Add UseModel

---
+++ UseModel.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+from musegan.core import MuseGAN
+from musegan.components import NowbarHybrid
+from config import *
+from input_data import InputDataNowBarHybrid
+
+# Initialize a tensorflow session
+with tf.Session() as sess:
+
+    # === Prerequisites ===
+    # Step 1 - Initialize the training configuration
+    t_config = TrainingConfig
+
+    # Step 2 - Select the desired model
+    model = NowbarHybrid(NowBarHybridConfig)
+
+    # Step 3 - Initialize the input data object
+    input_data = InputDataNowBarHybrid(model)
+
+    # Step 4 - Load training data
+    path_train = 'tra_bar.npy'
+    input_data.add_data(path_train, key='train')
+
+    # Step 5 - Initialize a museGAN object
+    musegan = MuseGAN(sess, t_config, model)
+
+    # === Training ===
+    musegan.train(input_data)
+
+    # === Load a Pretrained Model ===
+    musegan.load(musegan.dir_ckpt)
+
+    # === Generate Samples ===
+    path_test = 'tra_bar.npy'
+    input_data.add_data(path_test, key='test')
+    musegan.gen_test(input_data, is_eval=True)
